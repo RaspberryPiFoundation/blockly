25ba1b29d07d9d50e0da0dab6dfa3d5769275f78
heliacer <heliacer@gmx.ch>
2025-12-17 15:52:38 +0100

    fix: prettier problems

--- core/interfaces/i_json_block_definition.ts
+++ core/interfaces/i_json_block_definition.ts
@@ -4,32 +4,32 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { FieldCheckboxFromJsonConfig } from "../field_checkbox"
-import { FieldDropdownFromJsonConfig } from "../field_dropdown"
-import { FieldImageFromJsonConfig } from "../field_image"
-import { FieldNumberFromJsonConfig } from "../field_number"
-import { FieldTextInputFromJsonConfig } from "../field_textinput"
-import { FieldVariableFromJsonConfig } from "../field_variable"
+import {FieldCheckboxFromJsonConfig} from '../field_checkbox.js';
+import {FieldDropdownFromJsonConfig} from '../field_dropdown';
+import {FieldImageFromJsonConfig} from '../field_image';
+import {FieldNumberFromJsonConfig} from '../field_number';
+import {FieldTextInputFromJsonConfig} from '../field_textinput';
+import {FieldVariableFromJsonConfig} from '../field_variable';
 
 export interface JsonBlockDefinition {
-  type?: string
-  style?: string
-  colour?: string | number
-  output?: string | string[] | null
-  previousStatement?: string | string[] | null
-  nextStatement?: string | string[] | null
-  outputShape?: number
-  inputsInline?: boolean
-  tooltip?: string
-  helpUrl?: string
-  extensions?: string[]
-  mutator?: string
-  enableContextMenu?: boolean
-  suppressPrefixSuffix?: boolean
+  type?: string;
+  style?: string;
+  colour?: string | number;
+  output?: string | string[] | null;
+  previousStatement?: string | string[] | null;
+  nextStatement?: string | string[] | null;
+  outputShape?: number;
+  inputsInline?: boolean;
+  tooltip?: string;
+  helpUrl?: string;
+  extensions?: string[];
+  mutator?: string;
+  enableContextMenu?: boolean;
+  suppressPrefixSuffix?: boolean;
 
-  [key: `message${number}`]: string | undefined
-  [key: `args${number}`]: BlockArg[] | undefined
-  [key: `implicitAlign${number}`]: string | undefined
+  [key: `message${number}`]: string | undefined;
+  [key: `args${number}`]: BlockArg[] | undefined;
+  [key: `implicitAlign${number}`]: string | undefined;
 }
 
 /** Block Arg */
@@ -42,54 +42,54 @@ export type BlockArg =
   | FieldDropdownArg
   | FieldCheckboxArg
   | FieldImageArg
-  | FieldVariableArg
+  | FieldVariableArg;
 
 /** Input Args */
 interface InputValueArg {
-  name:  string
-  type: 'input_value'
-  check?: string | string[]
-  align?: FieldsAlign
+  name: string;
+  type: 'input_value';
+  check?: string | string[];
+  align?: FieldsAlign;
 }
 interface InputStatementArg {
-  name: string
-  type: 'input_statement'
-  check?: string | string[]
+  name: string;
+  type: 'input_statement';
+  check?: string | string[];
 }
 interface InputDummyArg {
-  name?: string
-  type: 'input_dummy'
+  name?: string;
+  type: 'input_dummy';
 }
 
 /** Field Args */
 interface FieldInputArg extends FieldTextInputFromJsonConfig {
-  name: string
-  type: 'field_input'
+  name: string;
+  type: 'field_input';
 }
 
 interface FieldNumberArg extends FieldNumberFromJsonConfig {
-  name: string
-  type: 'field_number'
+  name: string;
+  type: 'field_number';
 }
 
 interface FieldDropdownArg extends FieldDropdownFromJsonConfig {
-  name: string
-  type: 'field_dropdown'
+  name: string;
+  type: 'field_dropdown';
 }
 
 interface FieldCheckboxArg extends FieldCheckboxFromJsonConfig {
-  name: string
-  type: 'field_checkbox'
+  name: string;
+  type: 'field_checkbox';
 }
 
 interface FieldImageArg extends FieldImageFromJsonConfig {
-  name: string
-  type: 'field_image'
+  name: string;
+  type: 'field_image';
 }
 
 interface FieldVariableArg extends FieldVariableFromJsonConfig {
-  name: string
-  type: 'field_variable'
+  name: string;
+  type: 'field_variable';
 }
 
-export type FieldsAlign = 'LEFT' | 'RIGHT' | 'CENTRE' 
+export type FieldsAlign = 'LEFT' | 'RIGHT' | 'CENTRE';
