64df01198fff50490c5ca243c2e6cb42cb7561f2
Aaron Dodson <aaron.dodson@raspberrypi.org>
2026-04-17 13:26:49 -0700

    fix: Offset proposed top-level blocks during constrained drags

--- packages/blockly/core/dragging/block_drag_strategy.ts
+++ packages/blockly/core/dragging/block_drag_strategy.ts
@@ -479,6 +479,37 @@ export class BlockDragStrategy implements IDragStrategy {
     const newCandidate = this.getConnectionCandidate(delta);
 
     if (!newCandidate) {
+      // Position above or below the first/last block.
+      const connectedBlock = currCandidate?.neighbour.getSourceBlock();
+      let root = connectedBlock?.getRootBlock() ?? connectedBlock;
+      if (root === draggingBlock) root = connectedBlock;
+      const direction = this.getDirectionToNewLocation(
+        Coordinate.sum(this.startLoc!, delta),
+      );
+      const bounds = root?.getBoundingRectangle();
+      if (!bounds) return;
+
+      let destination: Coordinate;
+      switch (direction) {
+        case Direction.LEFT:
+        case Direction.UP:
+          destination = new Coordinate(
+            bounds.getOrigin().x,
+            bounds.getOrigin().y - 20 - draggingBlock.getHeightWidth().height,
+          );
+          break;
+        case Direction.RIGHT:
+        case Direction.DOWN:
+        default:
+          destination = new Coordinate(
+            bounds.getOrigin().x,
+            bounds.getOrigin().y + bounds.getHeight() + 20,
+          );
+          break;
+      }
+
+      draggingBlock.moveDuringDrag(destination);
+
       this.connectionPreviewer?.hidePreview();
       this.connectionCandidate = null;
       return;
