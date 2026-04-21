4bfa893d933a08b7a08fd90b495daf03c29ff96f
Mike Harvey <43474485+mikeharv@users.noreply.github.com>
2026-04-21 16:24:26 -0400

    feat: `FieldInput' ARIA

--- packages/blockly/core/field_input.ts
+++ packages/blockly/core/field_input.ts
@@ -175,6 +175,7 @@ export abstract class FieldInput<T extends InputTypes> extends Field<
     if (this.fieldGroup_) {
       dom.addClass(this.fieldGroup_, 'blocklyInputField');
     }
+    this.recomputeAriaContext();
   }
 
   override isFullBlockField(): boolean {
@@ -224,6 +225,7 @@ export abstract class FieldInput<T extends InputTypes> extends Field<
         );
       }
     }
+    this.recomputeAriaContext();
   }
 
   /**
@@ -238,6 +240,7 @@ export abstract class FieldInput<T extends InputTypes> extends Field<
     this.isDirty_ = true;
     this.isTextValid_ = true;
     this.value_ = newValue;
+    this.recomputeAriaContext();
   }
 
   /**
@@ -806,6 +809,46 @@ export abstract class FieldInput<T extends InputTypes> extends Field<
    */
   protected getValueFromEditorText_(text: string): AnyDuringMigration {
     return text;
+  }
+
+  /**
+   * Provide a default type if none is specified.
+   */
+  override getAriaTypeName(): string | null {
+    return this.ariaTypeName || Msg['ARIA_TYPE_FIELD_INPUT'];
+  }
+
+  /**
+   * Text inputs should always expose a meaningful value, even when empty.
+   */
+  override getAriaValue(): string | null {
+    return this.getText() || Msg['FIELD_LABEL_EMPTY_INPUT'];
+  }
+
+  /**
+   * Recomputes the ARIA role and label for this field.
+   */
+  private recomputeAriaContext(): void {
+    const focusableElement = this.getClickTarget_();
+    if (!focusableElement) return;
+
+    if (this.getSourceBlock()?.isInFlyout) {
+      aria.setState(focusableElement, aria.State.HIDDEN, true);
+      return;
+    }
+
+    aria.setState(focusableElement, aria.State.HIDDEN, false);
+    // The button role is intended to indicate to users that the field has an
+    // editing mode that can be activated.
+    focusableElement.setAttribute('role', 'button');
+
+    let label = this.computeAriaLabel(false);
+
+    if (this.isCurrentlyEditable?.()) {
+      label = Msg['FIELD_LABEL_EDIT_PREFIX'].replace('%1', label);
+    }
+
+    aria.setState(focusableElement, aria.State.LABEL, label);
   }
 }
 
