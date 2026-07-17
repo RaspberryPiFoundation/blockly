ca197b877c153d36eaf377d9fca1f8d4d17be8b5
Aaron Dodson <aaron.dodson@raspberrypi.org>
2026-07-17 15:20:45 -0700

    chore: Run formatter

--- packages/blockly/core/rendered_connection.ts
+++ packages/blockly/core/rendered_connection.ts
@@ -752,8 +752,7 @@ export class RenderedConnection
     // This cast is valid as TypeScript's definition is wrong. See:
     // https://github.com/microsoft/TypeScript/issues/60996.
     const root = this.getSourceBlock().getSvgRoot().getRootNode() as
-      | ShadowRoot
-      | HTMLDocument;
+      ShadowRoot | HTMLDocument;
     return root.getElementById(this.id) as SVGPathElement | null;
   }
 
